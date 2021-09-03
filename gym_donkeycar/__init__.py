e804b00aed4551eddab5e5e4aa8af7e7a02411db
naoe1999 <naoe1999@naver.com>
2021-09-03 10:44:01 +0900

    Initial commit

---
+++ gym_donkeycar/__init__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for OpenAI Gym Environments for Donkey Car."""
+
+__author__ = """Tawn Kramer"""
+__email__ = 'tawnkramer@gmail.com'
+__version__ = '1.0.16'
+
+from gym.envs.registration import register
+
+from .envs.donkey_env import (AvcSparkfunEnv, GeneratedRoadsEnv,
+                              GeneratedTrackEnv, MountainTrackEnv,
+                              RoboRacingLeagueTrackEnv, WarehouseEnv,
+                              WaveshareEnv, MiniMonacoEnv, WarrenTrackEnv)
+
+register(
+    id='donkey-generated-roads-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:GeneratedRoadsEnv',
+)
+
+register(
+    id='donkey-warehouse-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:WarehouseEnv',
+)
+
+register(
+    id='donkey-avc-sparkfun-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:AvcSparkfunEnv',
+)
+
+register(
+    id='donkey-generated-track-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:GeneratedTrackEnv',
+)
+
+register(
+    id='donkey-mountain-track-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:MountainTrackEnv',
+)
+
+register(
+    id='donkey-roboracingleague-track-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:RoboRacingLeagueTrackEnv',
+)
+
+register(
+    id='donkey-waveshare-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:WaveshareEnv',
+)
+
+register(
+    id='donkey-minimonaco-track-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:MiniMonacoEnv',
+)
+
+register(
+    id='donkey-warren-track-v0',
+    entry_point='gym_donkeycar.envs.donkey_env:WarrenTrackEnv',
+)
